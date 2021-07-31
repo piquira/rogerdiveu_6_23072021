f208aa3e10b78ee251e642c6cc8126a1573254bd
piquira <rogerdiveu@hotmail.com>
2021-07-31 10:40:23 +0200

    limiter ajout

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,15 @@
 //import
 const express = require('express');
 const router = express.Router();
+
+// Limitation du nombre de requêtes utilisateurs/temps
+const limiter = require('../middleware/limiter');
 // Contrôle si le courriel utilisateur est déja enregistrer
 const userCtrl = require('../controllers/user');
 // Controle si le mot de passe est conforme pour la sécurité
 const passwordValide = require('../middleware/password-valide');
 
 router.post('/signup', passwordValide, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', limiter, userCtrl.login);
 
 module.exports = router; 
